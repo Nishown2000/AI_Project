11fb05b90df2a4a60d6582091751c28bf5b84dbc
Nishown2000 <nishown2000@gmail.com>
2024-04-14 17:44:41 +0530

    AI model test

--- AI_Prompt/bot.py
+++ AI_Prompt/bot.py
@@ -1,18 +1,33 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
-model_name = "lmsys/fastchat-t5-3b-v1.0"
-tokenizer = AutoTokenizer.from_pretrained(model_name, legacy=False)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+model_id = "gg-hf/gemma-2b-it"
+dtype = torch.bfloat16
+access_token = "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"
 
-# User input
-user_input = "what is dog"
+# Initialize model without specifying device
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    token=access_token,
+    torch_dtype=dtype,
+)
 
-# Tokenize the input
-input_ids = tokenizer.encode(user_input, return_tensors="pt")
+# Set device to CUDA if available
+torch.cuda.empty_cache()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#torch.cuda.set_memory_growth(device, True)
 
-# Generate response
-output = model.generate(input_ids, max_length=50, num_return_sequences=1, no_repeat_ngram_size=2)
+# Move model to device
+model.to(device)
 
-# Decode and print the response
-response = tokenizer.decode(output[0], skip_special_tokens=True)
-print(response)
+tokenizer = AutoTokenizer.from_pretrained(model_id, token=access_token)
+
+chat = [
+    {"role": "user", "content": "where is india"},
+]
+prompt = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
+
+input_ids = tokenizer(prompt, add_special_tokens=False, return_tensors="pt").to(device)
+outputs = model.generate(input_ids, max_length=20)
+decoded_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
+print(decoded_output)
--- load_lib.sh
+++ load_lib.sh
@@ -3,11 +3,11 @@
 git-lfs install
 echo "lfs is intalled"
 
-if [ -d "FastChat_Lib" ]; then
-    echo "Library already exists. If not delete FastChat_Lib folder"
+if [ -d "Laama_Lib" ]; then
+    echo "Library already exists. If not delete Laama_Lib folder"
 else
-    GIT_LFS_SKIP_SMUDGE=1 git-lfs clone https://huggingface.co/lmsys/fastchat-t5-3b-v1.0 FastChat_Lib
-    echo "FastChat Library is Downloaded"
+    git-lfs clone https://Nishown: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@huggingface.co/meta-llama/Llama-2-7b-chat-hf Laama_Lib
+    echo "Laama_Lib Library is Downloaded"
 fi
 
 pip install torch torchvision torchaudio
